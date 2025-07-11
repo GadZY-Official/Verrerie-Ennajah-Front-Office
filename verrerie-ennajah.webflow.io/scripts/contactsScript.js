8eb16fb7142cf64b6b262af2bc06adf1218c47c8
GadZY-Team <gadzy.org@gmail.com>
2025-07-11 20:47:09 +0100

    v0.8

--- verrerie-ennajah.webflow.io/scripts/contactsScript.js
+++ verrerie-ennajah.webflow.io/scripts/contactsScript.js
@@ -5,12 +5,15 @@
 
     const doneElement = document.getElementById('done');
     const failElement = document.getElementById('fail');
+    const formElement = document.getElementById('contactForm');
 
     if (success) {
       if (doneElement) doneElement.style.display = 'block';
       if (failElement) failElement.style.display = 'none';
+      if (formElement) formElement.style.display = 'none';
     } else if (fail) {
       if (doneElement) doneElement.style.display = 'none';
       if (failElement) failElement.style.display = 'block';
+      if (formElement) formElement.style.display = 'none';
     }
   });
